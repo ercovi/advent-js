76fff1e097201b486647e065958b83c7c726c9a1
Èric <118215604+ercovi@users.noreply.github.com>
2023-12-01 17:23:01 +0100

    Delete reto-01.js

--- reto-01.js
+++
@@ -1,8 +0,0 @@
-function wrapping(gifts) {
-  const wrap = [];
-  gifts.forEach((gift) => {
-    const paper = '*'.repeat(gift.length+2);
-    wrap.push(`${paper}\n*${gift}*\n${paper}`);
-  })
-  return wrap;
-}
