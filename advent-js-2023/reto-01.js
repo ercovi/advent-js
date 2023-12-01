505e1ced9dde1edfb04d1ca00b5b98c49c2a3f99
Èric <118215604+ercovi@users.noreply.github.com>
2023-12-01 17:25:13 +0100

    Added reto-01 results

--- advent-js-2023/reto-01.js
+++ advent-js-2023/reto-01.js
@@ -1,10 +1,15 @@
 function findFirstRepeated(gifts) {
-    const empty = [];
-    for(let i = 0; i<gifts.length;i++){
-      if(empty.includes(gifts[i])){
-        return gifts[i];
-      }
-      empty.push(gifts[i])
+  const empty = [];
+  for(let i = 0; i<gifts.length;i++){
+    if(empty.includes(gifts[i])){
+      return gifts[i];
     }
-    return -1
+    empty.push(gifts[i])
   }
+  return -1
+}
+
+// RESULTS
+// Puntos conseguidos: 250
+// 3373 ops/s (Más alto es mejor)
+// Complejidad cognitiva: 3 (Más bajo es mejor)
