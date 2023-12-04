5731084a89bbc42789778156fde8f7d723b8ef0e
Èric Corbí <eric@llos.co>
2023-12-04 20:08:38 +0100

    Improved for second time the 2023 3rd challenge

--- advent-js-2023/reto-03/reto-03.js
+++ advent-js-2023/reto-03/reto-03.js
@@ -5,16 +5,13 @@ function findNaughtyStep(original, modified) {
   if(original === modified) return '';
 
   if(original.length > modified.length){
-    const originalChars = original.split('');
-    return originalChars.filter((char, i) => char !== modified[i])[0]
-  } else {
-    const modifiedChars = modified.split('');
-    return modifiedChars.filter((char, i) => char !== original[i])[0]
+    return original?.split('')?.filter((char, i) => char !== modified[i])[0]
   }
+  else return modified?.split('')?.filter((char, i) => char !== original[i])[0]
 }
 
 findNaughtyStep(original, modified);
 
-// Puntos conseguidos: 250
-// 2950 ops/s (Más alto es mejor)
+// Puntos conseguidos: 350
+// 3150 ops/s (Más alto es mejor)
 // Complejidad cognitiva: 5 (Más bajo es mejor)
