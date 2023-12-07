3be184d95d948aaed494bd8d792e9f66a34ef044
Èric Corbí <eric@llos.co>
2023-12-07 18:01:46 +0100

    FIX: Deleted logs and updated points

--- advent-js-2023/reto-04/reto-04.js
+++ advent-js-2023/reto-04/reto-04.js
@@ -8,19 +8,15 @@ function decode(message) {
 
   for (let i = 0; i < message.length; i++) {
     const char = message[i];
-    console.log({char})
     
     if (char === '(') {
       parts.push(endingResult);
       endingResult = '';
     } else if (char === ')') {
-      console.log({parts}, {endingResult})
       const lastArrayChar = parts.pop()
       endingResult = lastArrayChar + reverseWord(endingResult);
-      console.log({parts}, {endingResult})
     } else {
       endingResult += char;
-      console.log({endingResult})
     }
   }
 
@@ -28,7 +24,6 @@ function decode(message) {
 } 
 decode('sa(u(cla)atn)s')
 
-// Puntos conseguidos: 10
-// 578 ops/s (Más alto es mejor)
+// Puntos conseguidos: 220
+// 2210 ops/s (Más alto es mejor)
 // Complejidad cognitiva: 8 (Más bajo es mejor)
-// 1 usos de console detectados
