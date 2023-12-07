68b07a0bfc08c44a98d76f16af819bec58937d57
Èric Corbí <eric@llos.co>
2023-12-07 18:01:06 +0100

    FIX: Solved 4th 2023 challenge

--- advent-js-2023/reto-04/reto-04.js
+++ advent-js-2023/reto-04/reto-04.js
@@ -1,50 +1,34 @@
 function decode(message) {
-    if(!message.includes('(') || !message.includes(')')) return;
+  if(!message.includes('(') || !message.includes(')')) return;
+
+  const parts = [];
+  let endingResult = '';
+  
+  const reverseWord = (word) => word.split('').reverse().join('');
+
+  for (let i = 0; i < message.length; i++) {
+    const char = message[i];
+    console.log({char})
     
-    const words = message.split(' ');
-    console.log({words})
-    const reverseWord = (word) => word.split('').reverse().join('');
-    
-    let sentence = '';
-    words.forEach((word, i) => {
-      // console.log({word})
-      if(word.includes('(')){     
-        const open = message.indexOf('(') + 1;
-        const close = message.indexOf(')');
-        const open2 = message.lastIndexOf('(') + 1;
-        const close2 = message.lastIndexOf(')');
-        
-        const sliced = i == 0 ? message.slice(open, close) : message.slice(open2, close2);
-        // console.log({sliced})
-        // const sliced = message.substring(open, close);
-        const reversed = reverseWord(sliced)
-        sentence = sentence + ' ' + reversed
-      } else {
-        sentence = word;
-      }
-      
-      
-      // const toReverse = message.substring(open, close);
-      // console.log(toReverse, sliced)
-    })
-    return sentence.trim()
-    // const open = message.indexOf('(') + 1;
-    // const close = message.indexOf(')');
-    // console.log(message.substring(open, close))
-  //   const split = message.split('(')
-  //   const regex = /[^\w\s']/g;
+    if (char === '(') {
+      parts.push(endingResult);
+      endingResult = '';
+    } else if (char === ')') {
+      console.log({parts}, {endingResult})
+      const lastArrayChar = parts.pop()
+      endingResult = lastArrayChar + reverseWord(endingResult);
+      console.log({parts}, {endingResult})
+    } else {
+      endingResult += char;
+      console.log({endingResult})
+    }
   }
-  
-  // Paso a paso:
-  // 1. Invertimos el anidado -> sa(ualcatn)s
-  // 2. Invertimos el que queda -> santaclaus
-  
-  
-  // const a = decode('hola (odnum)')
-  // console.log(a) // hola mundo
-  
-  // const b = decode('(olleh) (dlrow)!')
-  // console.log(b) // hello world!
-  
-  const c = decode('sa(u(cla)atn)s')
-  console.log(c) // santaclaus
+
+  return endingResult;
+} 
+decode('sa(u(cla)atn)s')
+
+// Puntos conseguidos: 10
+// 578 ops/s (Más alto es mejor)
+// Complejidad cognitiva: 8 (Más bajo es mejor)
+// 1 usos de console detectados
