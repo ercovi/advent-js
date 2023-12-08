8dcb49070f3858e86469f66ed5645c9822cf7150
Èric Corbí <eric@llos.co>
2023-12-08 20:46:00 +0100

    Changed 5th 2023 challenge and only last test fails :(

--- advent-js-2023/reto-05/reto-05.js
+++ advent-js-2023/reto-05/reto-05.js
@@ -1,40 +1,41 @@
 function cyberReindeer(road, time) {
-    const timeline = [];
-    let iteration = 0;
-    let counter = 0;
-    
-    const sentence = road.split('');
-    const timer = setInterval(() => {
-      
-      if(road[iteration] === '|'){
-        if(counter > 4){
-          sentence[iteration] = 'S';
-          sentence[iteration - 1] = '*';
+  const timeline = [road];
+  let iteration = 1;
+  let lastChar = '';
   
-          const barriers = sentence.filter(char => char === '|');
-  
-          if(barriers.length > 0){
-            sentence.forEach((char, i) => sentence[i] === '|' ? sentence[i] = '*' : char = '');
-          }
-          timeline.push(sentence.join(''));
-        } else {
-          counter++;
-          return
-        }
-      } else if(road[iteration] === '.') {
+  const sentence = road.split('');
+
+  for(let i = 0; i < time - 1; i++ ){
+    console.log(road[i], road[iteration-1], lastChar,  lastChar === "|")
+    if(road[iteration] === '|'){
+      if(i > 3){
         sentence[iteration] = 'S';
         sentence[iteration - 1] = '.';
+        
+        const barriers = sentence.filter(char => char === '|');
+
+        if(barriers.length > 0){
+          sentence.forEach((char, i) => sentence[i] === '|' ? sentence[i] = '*' : char = '');
+        }
+        timeline.push(sentence.join(''));
+        iteration++;
+      } else {
         timeline.push(sentence.join(''));
       }
-      
-      if(counter === time) {
-        clearInterval(timer);
-        return timeline;
-      }
+    } else if(road[iteration] === '.') {
+      sentence[iteration] = 'S';
+      if(lastChar === "|") sentence[iteration - 1] = '*';
+      else sentence[iteration - 1] = '.';
+      timeline.push(sentence.join(''));
       iteration++;
-      counter++;
-    }, 1000)
+    }
+    
+    lastChar = road[iteration-1];
+
   }
   
-  
-  cyberReindeer('S..|...|..', 10)
+  return timeline;
+}
+
+
+cyberReindeer('S.||.|..', 10)
