b2f41abe6bd29fa772ce559a3e938e4132199f3c
Èric <85624552+Eric-llos@users.noreply.github.com>
2024-12-11 12:00:38 +0100

    add results to reto 01

--- advent-js-2024/reto-01/reto-01.js
+++ advent-js-2024/reto-01/reto-01.js
@@ -2,3 +2,9 @@ function prepareGifts(gifts) {
     const uniqueGifts = new Set([...gifts]);
     return Array.from(uniqueGifts).sort((a, b) => a - b)
 }
+
+
+// RESULTS
+// Puntos conseguidos: 5 estrellas
+//2117 ops/s (Más alto es mejor)
+//Complejidad cognitiva: 2 (Más bajo es mejor)
